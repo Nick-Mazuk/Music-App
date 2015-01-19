aaef22b95dbb9f466588fef0fd03d6f576b0a5b4
Nick-Mazuk <trainridernick@gmail.com>
2015-01-19 15:34:43 -0800

    Made it working
    Now the goal is to make it better. Currently, to make the edges, it is
    checking if the 0 is next to a 1, and making the 1 an edge. However, I
    think that if we check to see if the 1 is next to more than 1 edge, it
    would eliminate the extraneous points.

--- Edge Detection Test/script.js
+++ Edge Detection Test/script.js
@@ -1,6 +1,162 @@
 function edge() {
-	printArray(createArray(190));
-	
+	var size = 10;
+	var array = createArray(size);
+	printArray(array);
+	var finished = false;
+	array = fillArray(array);
+	console.log("----------------------");
+	printArray(array);
+	console.log("----------------------");
+	for(i = 0; i < size; i++) {
+		for(j = 0; j < size; j++) {
+			if(array[i][j] == " ") {
+				if(i == 0) {
+					if(j == 0) {
+						if(array[i][j+1] == "$") { //4
+							array[i][j+1] = "*";
+						}
+						if(array[i+1][j+1] == "$") { //5
+							array[i+1][j+1] = "*";
+						}
+						if(array[i+1][j] == "$") { //6
+							array[i+1][j] = "*";
+						}
+					} else if(j == size - 1) {
+						if(array[i+1][j] == "$") { //6
+							array[i+1][j] = "*";
+						}
+						if(array[i+1][j-1] == "$") { //7
+							array[i+1][j-1] = "*";
+						}
+						if(array[i][j-1] == "$") { //8
+							array[i][j-1] = "*";
+						}
+					} else {
+						if(array[i][j+1] == "$") { //4
+							array[i][j+1] = "*";
+						}
+						if(array[i+1][j+1] == "$") { //5
+							array[i+1][j+1] = "*";
+						}
+						if(array[i+1][j] == "$") { //6
+							array[i+1][j] = "*";
+						}
+						if(array[i+1][j-1] == "$") { //7
+							array[i+1][j-1] = "*";
+						}
+						if(array[i][j-1] == "$") { //8
+							array[i][j-1] = "*";
+						}
+					}
+
+				} else if(i == size - 1) {
+					if(j == 0) {
+						if(array[i-1][j] == "$") { //2
+							array[i-1][j] = "*";
+						}
+						if(array[i-1][j+1] == "$") { //3
+							array[i-1][j+1] = "*";
+						}
+						if(array[i][j+1] == "$") { //4
+							array[i][j+1] = "*";
+						}
+					} else if(j == size - 1) {
+						if(array[i-1][j-1] == "$") { //1
+							array[i-1][j-1] = "*";
+						}
+						if(array[i-1][j] == "$") { //2
+							array[i-1][j] = "*";
+						}
+						if(array[i][j-1] == "$") { //8
+							array[i][j-1] = "*";
+						}
+					} else {
+						if(array[i-1][j-1] == "$") { //1
+							array[i-1][j-1] = "*";
+						}
+						if(array[i-1][j] == "$") { //2
+							array[i-1][j] = "*";
+						}
+						if(array[i-1][j+1] == "$") { //3
+							array[i-1][j+1] = "*";
+						}
+						if(array[i][j+1] == "$") { //4
+							array[i][j+1] = "*";
+						}
+						if(array[i][j-1] == "$") { //8
+							array[i][j-1] = "*";
+						}
+					}
+				} else if(j == 0) {
+					if(array[i-1][j] == "$") { //2
+						array[i-1][j] = "*";
+					}
+					if(array[i-1][j+1] == "$") { //3
+						array[i-1][j+1] = "*";
+					}
+					if(array[i][j+1] == "$") { //4
+						array[i][j+1] = "*";
+					}
+					if(array[i+1][j+1] == "$") { //5
+						array[i+1][j+1] = "*";
+					}
+					if(array[i+1][j] == "$") { //6
+						array[i+1][j] = "*";
+					}
+				} else if(j == size - 1) {
+					if(array[i-1][j-1] == "$") { //1
+						array[i-1][j-1] = "*";
+					}
+					if(array[i-1][j] == "$") { //2
+						array[i-1][j] = "*";
+					}
+					if(array[i+1][j] == "$") { //6
+						array[i+1][j] = "*";
+					}
+					if(array[i+1][j-1] == "$") { //7
+						array[i+1][j-1] = "*";
+					}
+					if(array[i][j-1] == "$") { //8
+						array[i][j-1] = "*";
+					}
+				} else {
+					if(array[i-1][j-1] == "$") { //1
+						array[i-1][j-1] = "*";
+					}
+					if(array[i-1][j] == "$") { //2
+						array[i-1][j] = "*";
+					}
+					if(array[i-1][j+1] == "$") { //3
+						array[i-1][j+1] = "*";
+					}
+					if(array[i][j+1] == "$") { //4
+						array[i][j+1] = "*";
+					}
+					if(array[i+1][j+1] == "$") { //5
+						array[i+1][j+1] = "*";
+					}
+					if(array[i+1][j] == "$") { //6
+						array[i+1][j] = "*";
+					}
+					if(array[i+1][j-1] == "$") { //7
+						array[i+1][j-1] = "*";
+					}
+					if(array[i][j-1] == "$") { //8
+						array[i][j-1] = "*";
+					}
+				}
+			}
+		}
+	}
+
+	for(i = 0; i < size; i++) {
+		for(j = 0; j < size; j++) {
+			if(array[i][j] == "$") {
+				array[i][j] = " ";
+			}
+		}
+	}
+	printArray(array);
 }
 
 function createArray(size) {
@@ -8,7 +164,7 @@ function createArray(size) {
 	for(i = 0; i < size; i++) {
 		array[i] = [];
 		for(j = 0; j < size; j++) {
-			if(Math.random() < 0.1) {
+			if(Math.random() < 0.5) {
 				array[i][j] = "$";
 			} else {
 				array[i][j] = " ";
@@ -26,4 +182,185 @@ function printArray(array) {
 		}
 		console.log(string);
 	}
+}
+
+function fillArray(array) {
+	var size = array.length;
+	var done = false;
+	var time1 = new Date();
+	var StartTime = time1.getTime();
+	while(!done) {
+		done = true;
+		for(i = 0; i < size; i++) {
+			for(j = 0; j < size; j++) {
+				if(j == 0) {
+					if(i == 0) {
+						if(array[i][j+1] == "$" && array[i+1][j+1] == "$" && array[i+1][j] == "$" && array[i][j] == " ") {
+							array[i][j] == "$";
+							done = false;
+						} else if(array[i][j+1] == " " && array[i+1][j+1] == " " && array[i+1][j] == " " && array[i][j] == "$") {
+							array[i][j] == " ";
+							done = false;
+						}
+					} else if(i == size - 1) {
+						if(array[i][j+1] == "$" && array[i-1][j+1] == "$" && array[i-1][j] == "$" && array[i][j] == " ") {
+							array[i][j] == "$";
+							done = false;
+						} else if(array[i][j+1] == " " && array[i-1][j+1] == " " && array[i-1][j] == " " && array[i][j] == " $") {
+							array[i][j] == " ";
+							done = false;
+						}
+					} else {
+						var string = "";
+						string += array[i-1][j]; //2
+						string += array[i-1][j+1]; //3
+						string += array[i][j+1]; //4
+						string += array[i+1][j+1]; //5
+						string += array[i+1][j]; //6
+
+						//counts the number of "hits" around the pixel in question
+						var count = 0;
+						for(k = 0; k < string.length; k++) {
+							if(string.substring(k, k+1) == "$") {
+								count++;
+							}
+						}
+
+						if(count >= 6  && array[i][j] == " ") {
+							array[i][j] = "$";
+							done = false;
+						} else if(count == 0  && array[i][j] == "&") {
+							array[i][j] = " ";
+							done = false;
+						}
+					}
+				} else if (j == size - 1) {
+					if(i == 0) {
+						if(array[i][j-1] == "$" && array[i+1][j-1] == "$" && array[i+1][j] == "$" && array[i][j] == " ") {
+							array[i][j] == "$";
+							done = false;
+						} else if(array[i][j-1] == " " && array[i+1][j-1] == " " && array[i+1][j] == " " && array[i][j] == "$") {
+							array[i][j] == " ";
+							done = false;
+						}
+					} else if(i == size - 1) {
+						if(array[i][j-1] == "$" && array[i-1][j-1] == "$" && array[i-1][j] == "$" && array[i][j] == " ") {
+							array[i][j] == "$";
+							done = false;
+						} else if(array[i][j-1] == " " && array[i-1][j-1] == " " && array[i-1][j] == " " && array[i][j] == "$") {
+							array[i][j] == " ";
+							done = false;
+						}
+					} else {
+						var string = "";
+						string += array[i-1][j-1]; //1
+						string += array[i-1][j]; //2
+						string += array[i+1][j]; //6
+						string += array[i+1][j-1]; //7
+						string += array[i][j-1]; //8
+
+						//counts the number of "hits" around the pixel in question
+						var count = 0;
+						for(k = 0; k < string.length; k++) {
+							if(string.substring(k, k+1) == "$") {
+								count++;
+							}
+						}
+
+						if(count == 5 && array[i][j] == " ") {
+							array[i][j] = "$";
+							done = false;
+						} else if(count == 0 && array[i][j] == "$") {
+							array[i][j] = " ";
+							done = false;
+						}
+					}
+				} else if(i == 0) {
+					var string = "";
+					string += array[i][j+1]; //4
+					string += array[i+1][j+1]; //5
+					string += array[i+1][j]; //6
+					string += array[i+1][j-1]; //7
+					string += array[i][j-1]; //8
+
+					//counts the number of "hits" around the pixel in question
+					var count = 0;
+					for(k = 0; k < string.length; k++) {
+						if(string.substring(k, k+1) == "$") {
+							count++;
+						}
+					}
+
+					if(count == 5 && array[i][j] == " ") {
+						array[i][j] = "$";
+						done = false;
+					} else if(count == 0 && array[i][j] == "$") {
+						array[i][j] = " ";
+						done = false;
+					}
+
+				} else if(i == size - 1) {
+					var string = "";
+					string += array[i-1][j-1]; //1
+					string += array[i-1][j]; //2
+					string += array[i-1][j+1]; //3
+					string += array[i][j+1]; //4
+					string += array[i][j-1]; //8
+
+					//counts the number of "hits" around the pixel in question
+					var count = 0;
+					for(k = 0; k < string.length; k++) {
+						if(string.substring(k, k+1) == "$") {
+							count++;
+						}
+					}
+
+					if(count == 5 && array[i][j] == " ") {
+						array[i][j] = "$";
+						done = false;
+					} else if (count == 0 && array[i][j] == "$") {
+						array[i][j] = " ";
+						done = false;
+					}
+
+				} else { //NOT ON EDGE ________________________________________
+					var string = "";
+					string += array[i-1][j-1]; //1
+					string += array[i-1][j]; //2
+					string += array[i-1][j+1]; //3
+					string += array[i][j+1]; //4
+					string += array[i+1][j+1]; //5
+					string += array[i+1][j]; //6
+					string += array[i+1][j-1]; //7
+					string += array[i][j-1]; //8
+
+					//counts the number of "hits" around the pixel in question
+					var count = 0;
+					for(k = 0; k < string.length; k++) {
+						if(string.substring(k, k+1) == "$") {
+							count++;
+						}
+					}
+
+					if(count >= 6 && array[i][j] == " ") {
+						array[i][j] = "$";
+						done = false;
+					} else if(count == 0 && array[i][j] == "$") {
+						array[i][j] = " ";
+						done = false;
+					} else if(count == 4 && array[i][j] == "$") {
+						string += string;
+						if(string.indexOf("$ $ $ $ ") != -1) {
+							array[i][j] = "$";
+						}
+					}
+				}
+			}
+		}
+		time1 = new Date();
+		if(time1.getTime() >= StartTime + 5000) {
+			done = true;
+		}
+	}
+	return array;
 }
