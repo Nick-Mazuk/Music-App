e709ec16706f4afee120775581415d53dea10011
unknown <seno@CLAB-2-HP.ESUSD.esusd.k12.ca.us>
2015-04-21 12:01:21 -0700

    updated code

--- NoteCode/Note.java
+++ NoteCode/Note.java
@@ -2,6 +2,8 @@ public class Note {
 	private int pitch;
 	//duration is the denominator of the fraction of a whole note
 	private int duration;
+	private int volume;
+	private String status;
 
 	public Note(int pitch, int duration) {
 		setPitch(pitch);
@@ -16,9 +18,20 @@ public class Note {
 		return this.duration;
 	}
 
+	public int getVolume() {
+		return this.volume;
+	}
+
 	public void setPitch(int test) throws IllegalArgumentException {
 		if(test <= 127 && test >= 0)
 			this.pitch = test;
+		else
+			throw IllegalArgumentException();
+	}
+
+	public void setVolume(int test) throws IllegalArgumentException {
+		if(test <= 127 && test >= 0)
+			this.volume = test;
 		else
 			throw IllegalArgumentException();
 	}
@@ -28,6 +41,10 @@ public class Note {
 	}
 
 	public void play() {
-		
+		//playing the note code goes here
+	}
+
+	public String toString(int tempo) {
+		//convert this to a string
 	}
 }
