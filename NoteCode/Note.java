496b4b09229b1398164d5a959c98a245cd8c6468
unknown <seno@CLAB-2-HP.ESUSD.esusd.k12.ca.us>
2015-04-22 11:59:59 -0700

    added new code

--- NoteCode/Note.java
+++ NoteCode/Note.java
@@ -5,11 +5,43 @@ public class Note {
 	private int volume;
 	private String status;
 
+	//creates several default constructors
+	public Note() {
+		setPitch(36);
+		setDuration(4);
+		setVolume(127);
+		setStatus("FF");
+	}
+
+	public Note(int pitch) {
+		setPitch(pitch);
+		setDuration(4);
+		setVolume(127);
+		setStatus("FF");
+	}
+
 	public Note(int pitch, int duration) {
 		setPitch(pitch);
 		setDuration(duration);
+		setVolume(127);
+		setStatus("FF");
 	}
 
+	public Note(int pitch, int duration, String status) {
+		setPitch(pitch);
+		setDuration(duration);
+		setVolume(127);
+		setStatus(status);
+	}
+
+	public Note(int pitch, int duration, int volume, String status) {
+		setPitch(pitch);
+		setDuration(duration);
+		setVolume(volume);
+		setStatus(status);
+	}
+
+	//get attributes
 	public int getPitch() {
 		return this.pitch;
 	}
@@ -22,9 +54,21 @@ public class Note {
 		return this.volume;
 	}
 
+	public String getStatus() {
+		return this.status;
+	}
+
+	//set attributes
 	public void setPitch(int test) throws IllegalArgumentException {
 		if(test <= 127 && test >= 0)
 			this.pitch = test;
+		else
+			throw IllegalArgumentException();
+	}
+
+	public void setDuration(int test) throws IllegalArgumentException {
+		if(test > 0)
+			this.duration = test;
 		else
 			throw IllegalArgumentException();
 	}
@@ -36,14 +80,24 @@ public class Note {
 			throw IllegalArgumentException();
 	}
 
-	public void setDuration(int test) {
-		this.duration = test;
+	public void setStatus(String test) throws IllegalArgumentException { 
+		test = test.toUpperCase();
+		if(test.charAt(0) == 'F') {
+			if(test.charAt(1) == '8' || test.charAt(1) == '9' || (test.charAt(1) >= 'A' && (test.charAt(1) <= 'F')))
+				this.status = test;
+			else
+				throw IllegalArgumentException();
+		} else {
+			throw IllegalArgumentException();
+		}
 	}
 
+	//plays the note
 	public void play() {
 		//playing the note code goes here
 	}
 
+	//converts the note to a string
 	public String toString(int tempo) {
 		//convert this to a string
 	}
